da26c9ce6289f6eacc213ee9650e1d65cb779311
reshma-hussain <reshma.hussain@mittarv.com>
2025-11-19 15:54:05 +0530

    formatted sidebar

--- src/app/layout/sidebar/sidebar.ts
+++ src/app/layout/sidebar/sidebar.ts
@@ -1,8 +1,9 @@
-import { CdkMenu, CdkMenuItem, CdkMenuModule } from '@angular/cdk/menu';
+import { CdkMenu, CdkMenuItem } from '@angular/cdk/menu';
+import {MatDividerModule} from '@angular/material/divider';
 import { Component } from '@angular/core';
 
 @Component({
-  imports: [CdkMenu, CdkMenuItem],
+  imports: [CdkMenu, CdkMenuItem, MatDividerModule],
   selector: 'app-sidebar',
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
