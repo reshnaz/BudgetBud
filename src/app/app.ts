5c040536107758fa3ca629667308fa40c3471c30
reshma-hussain <reshma.hussain@mittarv.com>
2025-12-31 16:52:53 +0530

    Created the login form

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,13 +2,14 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Sidebar } from './structure/sidebar/sidebar';
 import { Layout } from './structure/layout/layout';
+import { Login } from "./login/login";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Sidebar, Layout],
+  imports: [RouterOutlet, Login],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('Budget Bud');
+  protected readonly title = signal('BudgetBud');
 }
--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ ReactiveFormsModule ],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
-
+  loginForm = new FormGroup ({
+    email: new FormControl(''),
+    password: new FormControl('')
+  })
 }
